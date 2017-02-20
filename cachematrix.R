f07f47cff583ecd496c7b4a8e019bec49fa46625
Taimur Aurora <taimur_07@yahoo.com>
2017-02-20 12:45:12 -0500

    Update and rename README.md to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,13 @@
 # cachematrix.R
 > ## Caching the Inverse of a Matrix: >
-## Matrix inversion is usually a costly computation and there may be some  >
-## benefit to caching the inverse of a matrix rather than compute it repeatedly. >
-## Below are a pair of functions that are used to create a special object that  > 
-## stores a matrix and caches its inverse. >  > 
-## This function creates a special "matrix" object that can cache its inverse. >  > 
+ 
 makeCacheMatrix &lt;- function(x = matrix()) 
 { +     inv &lt;- NULL +     set &lt;- function(y) 
 { +     x &lt;&lt;- y +         inv &lt;&lt;- NULL +     }
 +     get &lt;- function() x +     setInverse &lt;- function(inverse)
 inv &lt;&lt;- inverse +     getInverse &lt;- function() inv +     list(set = set, +     
 get = get, +          setInverse = setInverse, +          getInverse = getInverse) + } 
->  >  > ## This function computes the inverse of the special "matrix" created by  > 
-## makeCacheMatrix above. If the inverse has already been calculated (and the  >
-## matrix has not changed), then it should retrieve the inverse from the cache. >  > cacheSolve &lt;- function(x, ...) 
+  > cacheSolve &lt;- function(x, ...) 
 { +     ## Return a matrix that is the inverse of 'x' +     inv &lt;- x$getInverse() +     if (!is.null(inv)) 
 { +         message("getting cached data") 
 +         return(inv) +     } 
